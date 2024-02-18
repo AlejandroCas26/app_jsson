e3cd9646ba87f4755b2f8ca4638493c443a8b5e2
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-18 00:19:20 -0600

    Creando la estructura del proyecto y añadiendo el tarea.json

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:app_json/screens/user_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: UserListScreen(),
+    );
+  }
+}
