4d64c17a522d8191547671a6105c46aec306e6ba
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-18 00:34:43 -0600

    Creando las screens y creando el go_router

---
+++ lib/screens/user_list.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:app_json/services/service.dart';
+import 'package:app_json/screens/detalles_user.dart';
+
+class UserListScreen extends StatefulWidget {
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  late Future<List<dynamic>> _futureUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureUsers = UserService.getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: _futureUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final user = snapshot.data![index];
+                return ListTile(
+                  title: Text(user['name']),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => UserDetailsScreen(user: user),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
